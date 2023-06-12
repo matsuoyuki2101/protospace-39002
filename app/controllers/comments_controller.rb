a7d5860420431ed284d2648ecb6a74c477580f90
yuki1102anata <tnmm.ereviv@docomo.ne.jp>
2023-06-12 21:39:24 +0900

    First commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,4 +15,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end  
-end
+end 
